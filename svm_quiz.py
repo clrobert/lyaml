7629e1b72ee2d3909890dd3e1fb03d04fcd6b2dc
Chris Roberts <clrobert@uno.edu>
2016-11-20 16:08:29 -0600

    updated

--- svm_quiz.py
+++ svm_quiz.py
@@ -7,10 +7,12 @@
     Sara has label 0
     Chris has label 1
 """
+from udacity import *
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+sys.path.append("/udacity/tools/")
+
+from udacity.tools.email_preprocess import preprocess
 from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
@@ -18,8 +20,8 @@ from sklearn.svm import SVC
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 clf = SVC(C=10000.0, kernel="rbf")
 
@@ -30,6 +32,14 @@ print "training time:", round(time()-t0, 3), "s"
 t1 = time()
 pred = clf.predict(features_test)
 print "predicting time time:", round(time()-t1, 3), "s"
+
+print "Elements: ", pred[10], pred[26], pred[50]
+
+print len(pred)
+
+expected_chris = [ x for x in pred if x == 1 ]
+
+print len(expected_chris)
 
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
